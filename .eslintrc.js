4aa1ef43a3b3052abeea51d0233c7b4e9c77dd0a
akhmadiy <kodirov.a@payme.uz>
2022-04-08 18:20:41 +0500

    add ecma version to parser options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
+    ecmaVersion: 8,
     sourceType: "module",
   },
   plugins: ["@typescript-eslint/eslint-plugin"],
