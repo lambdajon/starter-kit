c73f959ed00298b71e7dc5dc81055a92604700f3
akhmadiy <kodirov.a@payme.uz>
2022-04-11 11:48:22 +0500

    fix context problem

--- src/interfaces/http/server.ts
+++ src/interfaces/http/server.ts
@@ -4,21 +4,14 @@ export class HttpServer {
   private port: number;
   private app: Express;
   constructor({ config }) {
-    console.log(config);
     this.port = config.application.http.port;
     this.app = express();
   }
-  start() {
-    // return new Promise((resolve) => {
-    // this.app.listen(this.port, () => {
-    //   console.log(`Application started on http://localhost${this.port}`);
-    //   resolve(true);
-    // });
-    // });
+  start = () => {
     return Promise.resolve().then(() => {
       this.app.listen(this.port, () => {
-        console.log(`Application started on http://localhost${this.port}`);
+        console.log(`Application started on http://localhost:${this.port}`);
       });
     });
-  }
+  };
 }
