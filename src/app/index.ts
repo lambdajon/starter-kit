f9c9bd18dd29af053875f220b4f48ec4098af65b
akhmadiy <kodirov.a@payme.uz>
2022-04-11 11:48:11 +0500

    remove trash comment code

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,5 @@
 import { createContainer, asClass, InjectionMode, asValue, asFunction } from 'awilix';
 import Config from '../config';
-import { InjectDependency } from './InjectDependency';
 import { AwlixContainer } from '../core/InjectableDependancies';
 import { MongodbDriver } from '../infra/mongo/driver';
 import { RedisDriver } from '../infra/redis/driver';
@@ -12,9 +11,6 @@ const container = createContainer<AwlixContainer>({
 });
 
 export default container.register({
-  // mongoDB: asFunction(
-  //   (dependencies) => new InjectDependency(dependencies, MongodbDriver, { inject: ['config'] })
-  // ).singleton() as any,
   mongoDB: asClass(MongodbDriver),
   redis: asClass(RedisDriver),
   httpServer: asClass(HttpServer).singleton(),
