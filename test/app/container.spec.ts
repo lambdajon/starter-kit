0497b1d6e114e21d2368f26fb20a6b06a11df11e
akhmadiy <kodirov.a@payme.uz>
2022-04-11 11:52:51 +0500

    add test for redis container

--- test/app/container.spec.ts
+++ test/app/container.spec.ts
@@ -3,20 +3,25 @@ import AppContainer from '../../src/app';
 import { Application } from '../../src/app/application';
 import { MongodbDriver } from 'src/infra/mongo/driver';
 import { HttpServer } from '../../src/interfaces/http/server';
-
+import {RedisDriver} from '../../src/infra/redis/driver';
 describe('Application container', () => {
   it('Should load database container', () => {
     const db = AppContainer.resolve<MongodbDriver>('mongoDB');
     expect(db).toBeDefined();
   });
 
-  it('Should http server container', () => {
+  it('Should load http server container', () => {
     const server = AppContainer.resolve<HttpServer>('httpServer');
     expect(server).toBeDefined();
   });
 
-  it('Should application container', () => {
+  it('Should load application container', () => {
     const app = AppContainer.resolve<Application>('application');
     expect(app).toBeDefined();
+  });
+
+  it('Should load redis container', () => {
+    const redis = AppContainer.resolve<RedisDriver>('redis');
+    expect(redis).toBeDefined();
   });
 });
