52c34c71791c9a82a2568aff1a7ca3e0cffce1bb
akhmadiy <ahmadulinjs@gmail.com>
2022-04-13 23:10:02 +0500

    refactor redisdb

--- src/infra/redis/index.ts
+++ src/infra/redis/index.ts
@@ -1,12 +1,9 @@
 import { RedisClientType } from 'redis';
 import ApplicationContainer from '../../app';
 
-// const redis = ApplicationContainer.cradle.redis.connection;
-
 export class RedisDB {
-  private db: any;
   static async create(path: string, key: string, value: string) {
-    const con = await ApplicationContainer.resolve('redis').connection();
-    return await con.hSet(path, key, value);
+    const connection: RedisClientType = await ApplicationContainer.resolve('redis').connection();
+    return await connection.hSet(path, key, value);
   }
 }
