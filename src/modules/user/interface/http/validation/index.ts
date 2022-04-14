f7d46320ee362921825fb226a3d9218cc275c909
akhmadiy <ahmadulinjs@gmail.com>
2022-04-14 07:25:51 +0500

    implement request validation

---
+++ src/modules/user/interface/http/validation/index.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+const createUserSchama = {
+  body: Joi.object({
+    firstName: Joi.string(),
+    lastName: Joi.string(),
+    email: Joi.string().email().required(),
+    phoneNumber: Joi.string().required(),
+    password: Joi.string().required()
+  })
+};
+
+export { createUserSchama };
